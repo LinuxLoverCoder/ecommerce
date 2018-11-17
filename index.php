fcefbbe722fd709ff5a6001279fe583918d72a4f
LinuxLoverCoder <brenosantanabruno@live.com>
2018-11-17 21:15:51 -0200

    CRUD Complete

--- index.php
+++ index.php
@@ -95,6 +95,15 @@ $app->get("/admin/users/:iduser/delete", function($iduser){
 
 		User::verifyLogin();
 
+		$user = new User();
+
+		$user->get((int)$iduser);
+
+		$user->delete();
+
+		header("Location: /admin/users");
+		exit;
+
 
 });
 
@@ -102,8 +111,13 @@ $app->get("/admin/users/:iduser/delete", function($iduser){
 $app->get("/admin/users/:iduser", function($iduser){
 
 	User::verifyLogin();
+	$user = new User();
+	$user->get((int)$iduser);
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+
+	));
 
 
 
@@ -131,6 +145,20 @@ $app->post("/admin/users/:iduser", function($iduser)
 {
 
 		User::verifyLogin();
+
+		$user = new User();
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+		$user->get((int)$iduser);
+
+		$user->setData($_POST);
+
+		$user->update();
+
+		header("Location: /admin/users");
+
+		exit;
 
 
 });
